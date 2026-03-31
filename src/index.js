6b33b5b802b55844275e6225ee5638d9966dc820
huynhtrongdai007 <huynhtrongdaiz@gmail.com>
2026-03-31 20:14:55 +0700

    post method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+
 //HTTP logger
 app.use(morgan('combined'));
 // Template engine
@@ -16,14 +21,21 @@ app.set('views',path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
+});
 
-})
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
